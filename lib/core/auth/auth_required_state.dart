eac2c88a13cb8ebef12335ebdcc64a311e4fad0c
Denys <dodenisa@gmail.com>
2022-02-05 22:12:27 +0200

    minor reorganization

--- lib/core/auth/auth_required_state.dart
+++ lib/core/auth/auth_required_state.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:task_manager/constants/supabase_constants.dart';
 
 class AuthRequiredState<T extends StatefulWidget>
     extends SupabaseAuthRequiredState<T> {
@@ -8,5 +9,13 @@ class AuthRequiredState<T extends StatefulWidget>
     if (mounted) {
       Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false);
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // todo maybe check for the role here and redirect to another page
+    // final profileUpsertResponse =
+    //     await supabase.from('profiles').select('role').execute();
   }
 }
--- lib/pages/temp/temp_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'temp_cubit.dart';
-
-@freezed
-class TempState with _$TempState {
-  factory TempState.loaded({
-    @Default(false) bool boolValue,
-    @Default('') String name,
-  }) = _Loaded;
-}
